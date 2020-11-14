1a9187a85e5ab87ae964e964ce30612942a3bb32
Andrzej Skrodzki <andrzej.skrodzki@gmail.com>
2020-11-14 18:05:36 +0100

    Create frames.

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 import streamlit as st
+from video import preprocessing
+import io
+import os
 
 st.title("AI hackaton")
 
@@ -7,5 +10,29 @@ uploaded_file = st.file_uploader("Choose a video...", type=["mp4"])
 if uploaded_file is not None:
     print("yeah!")
 
-    video_bytes = uploaded_file.read()
+    # video_bytes = uploaded_file.read()
+    # uploaded_file.
+    # st.video(video_bytes)
+
+    my_bar = st.progress(0)
+
+    output_path = "check.mp4"
+
+    preprocessing.upload_temporary(io.BytesIO(uploaded_file.read()), output_path)
+
+    video_file = open(output_path, 'rb')
+    video_bytes = video_file.read()
     st.video(video_bytes)
+
+    fps = preprocessing.get_fps(output_path)
+    st.text(f"FPS {fps}")
+
+    my_bar.progress(50)
+
+    os.makedirs('frames', exist_ok=True)
+
+    preprocessing.get_frames(output_path, 'frames', fps)
+
+    my_bar.progress(100)
+
+    st.text(f"Files {len(os.listdir('frames'))}")
---
+++ video/preprocessing.py
@@ -0,0 +1,27 @@
+import cv2
+
+
+def upload_temporary(file, path):
+    with open(path, 'wb') as out:
+        out.write(file.read())
+
+
+def get_fps(video_path):
+    video = cv2.VideoCapture(video_path)
+    return video.get(cv2.CAP_PROP_FPS)
+
+
+def get_frames(video_path, output_path, fps):
+    # Opens the Video file
+    cap = cv2.VideoCapture(video_path)
+    i = 0
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if not ret:
+            break
+        if i % fps == 0:
+            cv2.imwrite(f"{output_path}/{str(i // fps)}.jpg", frame)
+        i += 1
+
+    cap.release()
+    cv2.destroyAllWindows()
